70962fe53f0305c51e96567920a29d911c788477
James Fitzpatrick <james.fitzy86y@gmail.com>
2025-02-03 18:13:48 +0000

    updated css, added js for creating new html when bread recipe clicked

--- script.js
+++ script.js
@@ -11,3 +11,41 @@ hamburgerIcon.addEventListener("click", () => {
 function myFunction(x) {
     x.classList.toggle("change");
 };
+document.addEventListener("DOMContentLoaded", function(){
+    const breadRecipes = document.querySelectorAll(".bread-recipe");
+    const breadContainer = document.getElementById("bread-container");
+
+    
+    const newElement = document.createElement("div");
+    newElement.id = "recipeDetails";
+    newElement.style.display = "none";
+    newElement.innerHTML = `<p>Hidden element</p>`
+    breadContainer.after(newElement);
+
+    breadRecipes.forEach(recipe => {
+        recipe.addEventListener("click", function(){
+            newElement.style.display = "block";
+            for ( let i = 0;i < breadRecipes.length; i++){
+                breadRecipes[i].style.display = "none";
+            };
+            goBackButton.style.display = "block";
+            });
+        });
+    
+    
+    const goBackButton = document.createElement("button");
+    goBackButton.id = "go-back";
+    goBackButton.style.display = "none";
+    goBackButton.innerText = "Go Back";
+    newElement.after(goBackButton);
+
+    goBackButton.addEventListener("click",function(){
+        for(let recipe of breadRecipes){
+            recipe.style.display = "block";
+        };
+        newElement.style.display = "none";
+        goBackButton.style.display = "none";
+    });
+    
+    
+});
