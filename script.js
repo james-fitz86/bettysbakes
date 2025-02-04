981f39081d74da6179b04e348fecc71d4069c5a8
James Fitzpatrick <james.fitzy86y@gmail.com>
2025-02-04 21:13:27 +0000

    added array of objects to generate unique html for each bread-container div when clicked

--- script.js
+++ script.js
@@ -15,37 +15,53 @@ document.addEventListener("DOMContentLoaded", function(){
     const breadRecipes = document.querySelectorAll(".bread-recipe");
     const breadContainer = document.getElementById("bread-container");
 
-    
-    const newElement = document.createElement("div");
-    newElement.id = "recipeDetails";
-    newElement.style.display = "none";
-    newElement.innerHTML = `<p>Hidden element</p>`
-    breadContainer.after(newElement);
-
-    breadRecipes.forEach(recipe => {
-        recipe.addEventListener("click", function(){
-            newElement.style.display = "block";
-            for ( let i = 0;i < breadRecipes.length; i++){
-                breadRecipes[i].style.display = "none";
-            };
-            goBackButton.style.display = "block";
-            });
-        });
-    
-    
     const goBackButton = document.createElement("button");
     goBackButton.id = "go-back";
     goBackButton.style.display = "none";
     goBackButton.innerText = "Go Back";
-    newElement.after(goBackButton);
+    breadContainer.after(goBackButton);
 
-    goBackButton.addEventListener("click",function(){
-        for(let recipe of breadRecipes){
-            recipe.style.display = "block";
-        };
-        newElement.style.display = "none";
-        goBackButton.style.display = "none";
+    breadRecipes.forEach((recipe, index) => {
+        recipe.addEventListener("click", function(){
+            const newElement = document.createElement("div"); 
+            newElement.id = "recipeDetails";
+            newElement.style.display = "flex";
+
+            newElement.innerHTML = generateInnerHTML(index);
+            breadContainer.after(newElement);
+
+            for ( let i = 0;i < breadRecipes.length; i++){
+                breadRecipes[i].style.display = "none";
+            };
+            goBackButton.style.display = "block";
+
+            goBackButton.addEventListener("click",function(){ 
+                for(let recipe of breadRecipes){
+                    recipe.style.display = "block";
+                };
+                newElement.style.display = "none";
+                goBackButton.style.display = "none";
+            });
+        });
     });
+
+    function generateInnerHTML(index){
+
+        const recipes = [
+            {title: "Wheat Bread Recipe"},
+            {title: "Brown Bread Recipe"},
+            {title: "Rye Bread Recipe"},
+        ];
+
+        const recipe = recipes[index];
+
+        return `
+            <div>
+                <p>${recipe.title}</p>
+            </div>
+        `;
+
+    }
     
     
 });
