2959d2cf480746022aaa1bc0d46a7882d9b7daa4
James Fitzpatrick <james.fitzy86y@gmail.com>
2025-02-08 14:41:52 +0000

    bugfix of scaling orginal amounts

--- scale.js
+++ scale.js
@@ -1,5 +1,33 @@
+const hamburgerIcon = document.querySelector("#hamburger-icon");
+const nav = document.querySelector("nav");
+const main = document.querySelector("main");
+
+
+hamburgerIcon.addEventListener("click", () => {
+    nav.classList.toggle("nav-open");
+    main.classList.toggle("nav-open-main");
+    hamburgerIcon.classList.toggle("nav-open-icon");
+});
+function myFunction(x) {
+    x.classList.toggle("change");
+};
+
 let button = document.querySelector("#scale-button");
 button.addEventListener('click', scaleRecipe);
+
+document.addEventListener("DOMContentLoaded",storeOriginalAmounts);
+
+    function storeOriginalAmounts(){
+        let spans = document.querySelectorAll('span.amount');
+        let originalValues = [];
+
+        spans.forEach((span)=>{
+            originalValues.push(parseFloat(span.textContent))
+        });
+
+        localStorage.setItem("originalAmounts", JSON.stringify(originalValues));
+    };
+
 
 function scaleRecipe(){
     let originalArea = 264;
@@ -7,10 +35,11 @@ function scaleRecipe(){
     let typedWidth = width.value;
     const length = document.querySelector("#length-input");
     let typedLength = length.value;
-    let newArea = length.value * width.value;
+    let newArea = typedLength * typedWidth;
     let spans = document.querySelectorAll('span.amount');
-    for(span of spans){
-    let spanAmount = span.textContent;
-    span.innerText = Math.round((newArea / originalArea) * spanAmount);
-    }
-}
+    let originalValues = JSON.parse(localStorage.getItem("originalAmounts"));
+    spans.forEach((span, index)=>{
+    let originalAmount = originalValues[index];
+    span.innerText = Math.round((newArea / originalArea) * originalAmount);
+    });
+};
