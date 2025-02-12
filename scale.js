ca964675cb0cb33b78d4366d47500e5f877ddde8
James Fitzpatrick <james.fitzy86y@gmail.com>
2025-02-12 21:42:17 +0000

    added current versions of files as showing not same previous git push

--- scale.js
+++ scale.js
@@ -1,12 +1,14 @@
 const hamburgerIcon = document.querySelector("#hamburger-icon");
 const nav = document.querySelector("nav");
 const main = document.querySelector("main");
+const headerTitle = document.querySelector("#header-content");
 
 
 hamburgerIcon.addEventListener("click", () => {
     nav.classList.toggle("nav-open");
     main.classList.toggle("nav-open-main");
     hamburgerIcon.classList.toggle("nav-open-icon");
+    headerTitle.classList.toggle("nav-open-icon");
 });
 function myFunction(x) {
     x.classList.toggle("change");
