3e63c78431dd8412c76b1ffffc60a8cc7331c338
James Fitzpatrick <james.fitzy86y@gmail.com>
2025-02-15 14:48:32 +0000

    added JS for displaying full size image and navigating gallery

--- gallery.js
+++ gallery.js
@@ -1,14 +1,21 @@
 const thumbnails = document.querySelectorAll('.thumb');
 const gallery = document.getElementById('gallery');
+const galleryImage = document.getElementById('gallery-image');
+const close = document.querySelector('.close');
+const leftArrow = document.querySelector(".left");
+const rightArrow = document.querySelector(".right");
+
+let currentIndex = 0;
 
 thumbnails.forEach((thumb, index) =>{
     thumb.addEventListener('click', function(){
         gallery.style.display = 'flex';
-        console.log("Image", index);
+        galleryImage.src = this.getAttribute('alt');
+        currentIndex = index;
     })
 });
 
-const close = document.querySelector('.close');
+
 close.addEventListener('click', function(){
     gallery.style.display = 'none';
 });
@@ -16,5 +23,31 @@ close.addEventListener('click', function(){
 document.addEventListener('keydown', (event)=>{
     if(event.key === 'Escape'){
         gallery.style.display = 'none';
+    } else if(event.key === 'ArrowLeft'){
+        if (currentIndex > 0) {
+            currentIndex--;
+            galleryImage.src = thumbnails[currentIndex].getAttribute('alt');
+        }
+    } else if(event.key === 'ArrowRight'){
+        if (currentIndex < thumbnails.length - 1) {
+            currentIndex++;
+            galleryImage.src = thumbnails[currentIndex].getAttribute('alt');
+        }
+    }
+});
+
+leftArrow.addEventListener('click', function(){
+    console.log("left arrow clicked");
+    if (currentIndex > 0) {
+        currentIndex--;
+        galleryImage.src = thumbnails[currentIndex].getAttribute('alt');
+    }
+});
+
+rightArrow.addEventListener('click', function(){
+    console.log("right arrow clicked");
+    if (currentIndex < thumbnails.length - 1) {
+        currentIndex++;
+        galleryImage.src = thumbnails[currentIndex].getAttribute('alt');
     }
 });
