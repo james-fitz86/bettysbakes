e0b09c0c85155e67d2edbf9c86bdd8d007ff96c6
James Fitzpatrick <james.fitzy86y@gmail.com>
2025-02-16 19:53:27 +0000

    added JS for quick recipes, navaigate through them plus add items to a shopping list

---
+++ quick.js
@@ -0,0 +1,85 @@
+const quickRecipes = document.querySelectorAll('.quick-recipes');
+const quickContainer = document.getElementById('quick-container');
+const previousArrow = document.querySelector(".previous");
+const nextArrow = document.querySelector(".next");
+const listButtton = document.querySelector("#add-to-shopping-list");
+const shoppingListItems = document.getElementById('shopping-items');
+const clearListButton = document.querySelector('#clear-shopping-list');
+const shoppingList = document.getElementById('shopping-list');
+const closeListButton = document.querySelector('#close-shopping-list');
+const viewListButton = document.querySelector('#view-shopping-list');
+const printButton = document.querySelector('#print');
+const printList = document.querySelector('#printing-list');
+
+let presentIndex = 0;
+
+function updateDisplay() {
+    quickRecipes.forEach((recipe, index) => {
+        if (index === presentIndex) {
+            recipe.style.display = 'block';
+          } else {
+            recipe.style.display = 'none';
+          }
+    });
+};
+
+updateDisplay();
+
+previousArrow.addEventListener('click', function(){
+    console.log("previous clicked");
+    if (presentIndex > 0) {
+        presentIndex--;
+        updateDisplay();
+    }
+});
+
+nextArrow.addEventListener('click', function(){
+    console.log("next clicked");
+    if (presentIndex < quickRecipes.length - 1) {
+        presentIndex++;
+        updateDisplay();
+    };
+});
+
+quickRecipes.forEach(recipe => {
+    const listItems = recipe.querySelectorAll('ul li');
+    listItems.forEach(item => {
+        item.addEventListener('click', () => {
+            item.classList.toggle('marked');
+        });
+    });
+});
+
+
+listButtton.addEventListener('click', function(){
+    quickRecipes.forEach(recipe => {
+        const markedItems = recipe.querySelectorAll('ul li.marked');
+        markedItems.forEach(item => {
+            const listItem = document.createElement('li');
+            listItem.textContent = item.textContent;
+            shoppingListItems.appendChild(listItem);
+            item.classList.remove('marked');
+        });
+    });
+});
+
+clearListButton.addEventListener('click', function(){
+    shoppingListItems.innerText = "";
+});
+
+
+closeListButton.addEventListener('click', function(){
+    shoppingList.style.display = "none";
+});
+
+viewListButton.addEventListener('click', function(){
+    shoppingList.style.display = "block";
+});
+
+printButton.addEventListener('click', function(){
+    const printWindow = window.open('', '', 'height=500,width=800');
+    printWindow.document.write(printList.innerHTML);
+    printWindow.document.close();
+    printWindow.print();
+});
+
